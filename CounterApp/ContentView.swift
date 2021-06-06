cc1a0593a942c4cedd6a49eb0538e81c7ae257ac
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-06 21:52:01 +0900

    finish 7

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -18,10 +18,15 @@ struct ContentView: View {
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 Text("\(number)")
                     .foregroundColor(Color.white)
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
                     .padding()
             }
             Button(action: {self.number += 1}) {
                 Text("カウント")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(10.0)
             }
         }
     }
